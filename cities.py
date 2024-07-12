382a1586c2d68bd92493f511bb763f63823b0341
gfranco7 <deepagmf710@gmail.com>
2024-07-12 16:19:57 -0500

    feat: Creacion de la funcion agregar ciudad

--- cities.py
+++ cities.py
@@ -1,2 +1,18 @@
+def create_city(datos_1):
+    datos_1 = dict(datos_1)
+    city={}
+    try:
+        city["codigo_postal"]=input("Ingrese el código postal de la ciudad: ")
+    except Exception:
+        city["codigo_postal"] = 0
+        print("Codigo inválido. Para modificarlo ingrese en la opción -actualizar perfil de usuarios- ")
 
+    city["nombre"]=input("Ingrese el nombre de la ciudad: ")
+    city["poblacion"]=input("Ingrese la poblacion de la ciudad: ")
+    city["pais"]=input("Ingrese el pais : ")
+    
+    datos_1["cities"].append(city)
+    print("-------------------------------------")
+    print("El registro se ha completado con éxito")
+    print("-------------------------------------")
 
--- menu.py
+++ menu.py
@@ -1,1 +1,11 @@
-def menu_principal()
+from cities import *
+def menu_principal(datos_1):
+    print("- -  Bienvenido al sistema  - -")
+    print("- -  [1] Para agregar una ciudad - -")
+    print("- -  [2] Para modificar información de una ciudad - -")
+    print("- -  [3] Para ver informe de ciudades - -")
+    print("- -  [3] Para ver informe de ciudades - -")
+    opcion = int(input("- -  Ingrese la opcion deseada - : "))
+    if opcion == 1:
+        create_city(datos_1)
+    else: print("-----------------------------------------------------------------")
