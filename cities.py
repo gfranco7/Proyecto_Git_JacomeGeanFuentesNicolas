4aab26e1fa447b1889bc6098e46d3f4f3d78697a
gfranco7 <deepagmf710@gmail.com>
2024-07-12 16:45:29 -0500

    feat: creacion de la funcion modificar cities

--- cities.py
+++ cities.py
@@ -16,3 +16,38 @@ def create_city(datos_1):
     print("El registro se ha completado con éxito")
     print("-------------------------------------")
 
+def update_city(datos_1):
+    datos_1 = dict(datos_1)
+    city={}
+    
+    for i in (datos_1["cities"]):
+        city.append(i["codigo_postal"])
+        
+    print(city)
+    
+    contador_cities = 0
+    city_id = int(input("Ingrese el código postal de la ciudad que desea modificar = "))
+    
+    for i in (datos_1["cities"]):
+            if city_id in city and city_id == i["codigo_postal"]:
+                contador_cities += 1
+                new_name =str(input("Actualizar el nombre. De lo contrario escriba -0- :"))
+                if new_name == "0":
+                         print(" ")
+                else: i["nombre"]=new_name
+                new_population =str(input("Actualizar la poblacion. De lo contrario escriba -0- :"))
+                if new_population == "0":
+                         print(" ")
+                else: i["poblacion"]=new_population
+                new_country =str(input("Actualizar el pais. De lo contrario escriba -0- :"))
+                if new_country == "0":
+                         print(" ")
+                else: i["pais"]=new_country
+    datos_1["cities"].append(city)
+    print("-------------------------------------")
+    print("La actualizacion de datos se ha completado con éxito")
+    print("-------------------------------------")
+    if contador_cities == 0:
+        print("Usuario no encontrado")         
+    update_city(datos_1)
+
--- menu.py
+++ menu.py
@@ -1,11 +1,20 @@
 from cities import *
 def menu_principal(datos_1):
-    print("- -  Bienvenido al sistema  - -")
-    print("- -  [1] Para agregar una ciudad - -")
-    print("- -  [2] Para modificar información de una ciudad - -")
-    print("- -  [3] Para ver informe de ciudades - -")
-    print("- -  [3] Para ver informe de ciudades - -")
-    opcion = int(input("- -  Ingrese la opcion deseada - : "))
-    if opcion == 1:
-        create_city(datos_1)
-    else: print("-----------------------------------------------------------------")
+    while True: 
+        print("- -  Bienvenido al sistema  - -")
+        print("- -  [1] Para agregar una ciudad - -")
+        print("- -  [2] Para modificar información de una ciudad - -")
+        print("- -  [3] Para ver informe de ciudades - -")
+        print("- -  [4] Para salir del sistema - -")
+        opcion = int(input("- -  Ingrese la opcion deseada - : "))
+        if opcion == 1:
+            create_city(datos_1)
+        elif opcion == 2:
+            update_city(datos_1)
+        elif opcion == 3:
+            reporte_ciudades(datos_1)
+        elif opcion == 4:
+            print(" -- Saliendo del sistema... --")
+            break
+
+    
